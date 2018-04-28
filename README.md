fc2678723df2e0e4f609da8d3a809c7496ee773d
Spiegel <spiegel.im.spiegel@gmail.com>
2018-04-28 22:48:05 +0900

    Update database spec

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# jvnman
-JVN Data management
+# [jvnman] - JVN Data management
+
+[jvnman] は [JVN] が提供する「[脆弱性対策情報共有フレームワーク]」を使った脆弱性情報の収集・管理ツールです。
+
+
+
+
+
+
+## 参考
+
+- [MyJVN API に関する覚え書き — しっぽのさきっちょ | text.Baldanders.info](http://text.baldanders.info/remark/2018/03/myjvn-api/)
+- [spiegel-im-spiegel/go-myjvn: Handling MyJVN RESTful API by Golang](https://github.com/spiegel-im-spiegel/go-myjvn)
+
+[jvnman]: https://github.com/spiegel-im-spiegel/jvnman "spiegel-im-spiegel/jvnman: JVN Data management"
+[JVN]: https://jvn.jp/ "Japan Vulnerability Notes"
+[脆弱性対策情報共有フレームワーク]: https://jvndb.jvn.jp/apis/myjvn/ "脆弱性対策情報共有フレームワーク - MyJVN"
--- database/create.go
+++ database/create.go
@@ -2,11 +2,11 @@ package database
 
 var stmtsCreate = []string{
 	"pragma auto_vacuum = full;",
-	"create table vulnlist (id text not null primary key, title text, description text, uri text, creator text, impact text, solution text, date_public integer not null, date_publish integer not null, date_update integer not null);",
+	"create table vulnlist (id text not null primary key, title text, description text, uri text, creator text, impact text, solution text, date_public integer, date_publish integer, date_update integer);",
 	"create table affected (id text not null, name text not null, product_name text not null, version_number text not null, primary key (id, name, product_name, version_number));",
 	"create table cvss (id text not null, version text not null, base_vector text, base_score numeric, severity text, primary key (id, version));",
-	"create table related (id text not null, type text, name text, vulinfo_id text not null, title text, url text, primary key (id, type, name, vulinfo_id));",
-	"create table history (id text not null, history_no numeric not null, description text, date_time integer not null, primary key (id, history_no));",
+	"create table related (id text not null, type text not null, name text not null, vulinfo_id text not null, title text, url text, primary key (id, type, name, vulinfo_id));",
+	"create table history (id text not null, history_no numeric not null, description text, date_time integer, primary key (id, history_no));",
 	"create view vulnview as select list.id as id, list.title as title, list.description as description, list.uri as uri, list.impact as impact, list.solution as solution, cvss.base_score as cvss_score, cvss.severity as cvss_severity, list.date_public as date_public, list.date_publish as date_publish, list.date_update as date_update from vulnlist list left outer join cvss on list.id = cvss.id and cvss.version = '3.0';",
 }
 
